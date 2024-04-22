0ed64086d047106008ecfdf858bc8f6a28e8068c
rcedi1012 <166011260+rcedi1012@users.noreply.github.com>
2024-04-22 13:57:19 -0500

    Update Once_Upon_an_Arduino.ino
    added some comments

--- Once_Upon_an_Arduino.ino
+++ Once_Upon_an_Arduino.ino
@@ -1,60 +1,73 @@
 #include <Servo.h>
 
+//add servos
 Servo servo1;
 Servo servo2;
 Servo servo3;
 
-const int switchPin = 11;
+//create button and switch variables
+const int switchPin = 13; //1
 int switchState = 0;
 int previousSwitchState = 0;
 
-const int buttonPin = 12;
+const int buttonPin = 12;//2
 int buttonState = 0;
 int previousButtonState = 0;
 
-const int slidePin = 13;
+/*
+Not in use
+const int slidePin = 1;
 int slideState = 0;
 int previousSlideState = 0;
+*/
 
-const int spikePin = 13;
+const int spikePin = 11;//3
 int spikeState = 0;
 int previousSpikeState = 0;
 
-const int led1 = 6;
-const int led2 = 5;
-const int led3 = 4;
+int led1 = (6); //led pin
 
+bool swish = true; //keep the tail swishing indefinitely
 
 void setup() {
-  servo1.attach(8);
-  servo2.attach(9);
-  servo3.attach(10);
+  servo1.attach(8); //servo 1 pin on 8
+  servo2.attach(9); //servo 2 pin on 9
+  servo3.attach(10); //servo 3 pin on 10
 
+  //inputs
   pinMode(switchPin, INPUT);
   pinMode(buttonPin, INPUT);
   pinMode(slidePin, INPUT);
 
+  //output
   pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
 
-  //Servo Reset
+  //Servo and led Reset
 
   servo1.write(90);
   servo2.write(0);
-  servo3.write(0);
+
+  digitalWrite(led1, HIGH); //inverted because it wasn't working the correct way
 
 }
 
 void loop() {
+
+  if (swish == true) { //tail swish loop
+    servo3.write(170);
+    delay(1000);
+    servo3.write(10);
+    delay(1000);
+  }
+  
  
-// Baby Godzilla swim to right (manual) and toxic waste can rotate to pour waste on godzilla (servo for can (45) and slide switch)
+// Baby Godzilla swim to right (manual) and toxic waste can rotate to pour waste on godzilla (servo for can (0) and slide switch)
 
   switchState = digitalRead(switchPin);
 
-  if (switchState != previousSwitchState) {
+  if (switchState != previousSwitchState) { //when the slide mechanism connects the pieces of copper tape, then the servo will rotate to angle 0
     if (switchState == HIGH) {
-       servo1.write(180);
+       servo1.write(0); //rotating to angle 0 makes it look like the toxic waste can is pouring on top of Godzilla
     }
   }
   previousSwitchState = switchState;
@@ -63,7 +76,7 @@ void loop() {
 
  buttonState = digitalRead(buttonPin);
 
-  if (buttonState != previousButtonState) {
+  if (buttonState != previousButtonState) { // when the button is pushed, godzilla flips
     if (buttonState == HIGH) {
        servo2.write(180);
     }
@@ -73,28 +86,13 @@ void loop() {
 
 // Godzilla rising (Manual)
   
-
-
-// Godzilla breaking through the bridge (servo for tail swish (180))
-  
-  slideState = digitalRead(slidePin);
-
-  if (slideState != previousSlideState) {
-    if (slideState == HIGH) {
-       servo3.write(180);
-    }
-  }
-  previousSlideState = slideState;
-
 // Godzilla atomic breath (leds)
 
   spikeState = digitalRead(spikePin);
 
-  if (spikeState != previousSpikeState) { //will change when i figure out how to make each one light up independently without delay
+  if (spikeState != previousSpikeState) { //light up led when spike is pulled out
     if (spikeState == HIGH) {
-      digitalWrite(led1, HIGH);
-      digitalWrite(led2, HIGH);
-      digitalWrite(led3, HIGH);
+      digitalWrite(led1, LOW);
     }
   }
   previousSlideState = slideState;
